e8f98b1abd911ac3118dd98e4efc809a716d16d9
Harshita-shree08 <shreeprada1504@gmail.com>
2026-09-22 11:41:29 +0530

    Delete Agecalc.java

--- Agecalc.java
+++
@@ -1,8 +0,0 @@
-public class Agecalc{
-	public static void main(String[] args){
-int birthyr = 2000;
-int currentyr = 2024;
-int age = currentyr - birthyr;
-System.out.println("Harry's age is:" + age);
-	}
-}	
