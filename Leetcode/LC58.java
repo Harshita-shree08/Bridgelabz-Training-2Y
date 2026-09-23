4a8bc81b827e40d4fbb09cb1a35b81cdb3cc530f
Harshita-shree08 <shreeprada1504@gmail.com>
2026-09-23 22:41:54 +0530

    problem 58'

---
+++ Leetcode/LC58.java
@@ -0,0 +1,30 @@
+
+public class LC58 {
+
+    public static int lengthOfLastWord(String s) {
+
+        s = s.trim();
+
+        int count = 0;
+
+        for (int i = s.length() - 1; i >= 0; i--) {
+
+            if (s.charAt(i) == ' ') {
+                break;
+            }
+
+            count++;
+        }
+
+        return count;
+    }
+
+    public static void main(String[] args) {
+
+        String s = "Hello World";
+
+        int result = lengthOfLastWord(s);
+
+        System.out.println("Length of last word: " + result);
+    }
+}
