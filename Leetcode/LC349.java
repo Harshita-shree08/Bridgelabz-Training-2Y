ca87ceff3478e95c75923ee7fdb5569203769500
Harshita-shree08 <shreeprada1504@gmail.com>
2026-09-23 23:02:35 +0530

    problem 349'

---
+++ Leetcode/LC349.java
@@ -0,0 +1,43 @@
+
+import java.util.*;
+
+public class LC349 {
+
+    public static int[] intersection(int[] nums1, int[] nums2) {
+
+        HashSet<Integer> set1 = new HashSet<>();
+
+        for (int num : nums1) {
+            set1.add(num);
+        }
+
+        HashSet<Integer> resultSet = new HashSet<>();
+
+        for (int num : nums2) {
+            if (set1.contains(num)) {
+                resultSet.add(num);
+            }
+        }
+
+        int[] result = new int[resultSet.size()];
+
+        int i = 0;
+
+        for (int num : resultSet) {
+            result[i] = num;
+            i++;
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+
+        int[] nums1 = {1, 2, 2, 1};
+        int[] nums2 = {2, 2, 3};
+
+        int[] result = intersection(nums1, nums2);
+
+        System.out.println("Intersection: " + Arrays.toString(result));
+    }
+}
